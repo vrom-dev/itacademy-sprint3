df162b6e016676e84ce66e6d9fff9eb51a9d8885
Víctor Romero <vromerocomino@gmail.com>
2021-07-14 10:49:31 +0200

    part3: nivell3 observer pattern with events module

--- patterns1/nivell3/index.js
+++ patterns1/nivell3/index.js
@@ -1,6 +1,9 @@
+const EventEmitter = require('events')
+
 class Observable  {
   constructor() {
     this.observers = []
+    this.emitter = new EventEmitter()
   }
 
   subscribe(o) {
@@ -11,8 +14,8 @@ class Observable  {
     this.observers = this.observers.filter(observer => observer !== o)
   }
 
-  notify(model) {
-    this.observers.forEach(observer => observer.notify(model))
+  addListener(name, cb) {
+    this.emitter.on(name, cb)
   }
 }
 
@@ -20,11 +23,13 @@ class Topic extends Observable {
   constructor() {
     super()
     this.messages = []
+    this.addListener('setMessage', (model) => this.observers.forEach(observer => observer.notify(model)))
   }
   setMessage(msg, user) {
     const messageToAdd = {user, message: msg}
     this.messages = [...this.messages, messageToAdd]
-    this.notify(messageToAdd)
+    console.log('New message added!')
+    this.emitter.emit('setMessage', messageToAdd)
   }
 }
 
